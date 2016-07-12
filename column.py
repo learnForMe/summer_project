a8fc107d18496cd975279f0e26c54b13ef83c017
garymalaysia <gtsai70@gmail.com>
2016-07-12 19:24:01 -0400

    completed with row A1 and B1

--- column.py
+++ column.py
@@ -4,21 +4,6 @@ from openpyxl import load_workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter, column_index_from_string
 
-f=open ('lastday.txt', 'r')
-text=f.read()
-#texting=text.split('/n')
-texting ="g"
-
-wb=load_workbook('testing.xlsx',read_only = False, data_only = True)
-sheet = wb.get_sheet_by_name('Sheet1')
-ws=wb.active
-count =0
-column=1
-col=get_column_letter(column)
-
-max_row = sheet.max_row
-insert_name=max_row+1
-
 #insert_name=max_row+1
 #print insert_name
 
@@ -26,25 +11,56 @@ insert_name=max_row+1
 
 #row=1
 #column=4
-'''
-for i in range(1,insert_name):
-	new=(i,sheet.cell(row=i,column=1).value)
-	print new
-	'''
-for row in sheet.iter_rows(): 
-        for cell in row:
-            data = cell.value 
-            print data
-            if data == str(texting):
+def search_Student (x):
+	count =0
+	for row in sheet.iter_rows():
+		for cell in row:
+			data = cell.value
+			#print data
+			if data == str(texting):
 				count+=1
 				
-if count< 1:
-	print "This Student is NEW"
-	ws['%s%d' % (col,insert_name)] =str(texting)
-	wb.save('testing.xlsx')
-else:
-	print "Not New"            	
+	if count< 1:
+		print "This Student is NEW"
+		student_name=raw_input("Enter Name -> ")
+		ws['%s%d' % (col2,insert_name)] =str(student_name)
+		ws['%s%d' % (col,insert_name)] =str(texting)
 
-            	
+		wb.save('testing.xlsx')
+	else:
+		print "Welcome Back!"
+		return data
+
+
+
+def one_day_increment (y):
+	max_col=sheet.max_col
+	#day=
+
+f=open ('lastday.txt', 'r')
+text=f.read()
+texting=text.split('/n')
+#texting ="g"
+
+wb=load_workbook('testing.xlsx',read_only = False, data_only = True)
+sheet = wb.get_sheet_by_name('Sheet1')
+ws=wb.active
+
+col=get_column_letter(1)# convert column number to letter and use for first column (ID card data)
+col2=get_column_letter(2)# use for second column (Student name)
+col3=get_column_letter(3)#use for third column(occurance)
+
+max_row = sheet.max_row
+insert_name=max_row+1
+    
+search_Student(texting)        	
 #wb.save('testing.xlsx')
+
 f.closed
+
+
+
+
+
+
+
