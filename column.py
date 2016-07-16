0fff3678ec37bf75f434bef7a08b2ea05d121414
garymalaysia <gtsai70@gmail.com>
2016-07-16 15:08:19 -0400

    able to correspond between cell and it's row

--- column.py
+++ column.py
@@ -3,10 +3,21 @@ import openpyxl
 from openpyxl import load_workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter, column_index_from_string
+from openpyxl.utils import coordinate_from_string
+import re
 
 #insert_name=max_row+1
 #print insert_name
 
+def remove(y):
+	#cell_location=re.sub(r'<','',y)
+	#cell_location=re.sub(r'Cell','',cell_location)
+	cell_location=re.sub(r'[< Cell Sheet1.]','',y)
+	cell_location=re.sub(r'>','',cell_location)
+	cell_location=re.sub(r'[A-Z]','',cell_location)
+
+	#cell_location=re.sub(r'[< Cell Sheet1]','',cell_location)
+	return cell_location
 
 
 #row=1
@@ -17,11 +28,14 @@ def search_Student (x):
 	for row in sheet.iter_rows():
 		for cell in row:
 			data = cell.value
-			#print data
-			if data == str(texting):
-				stop_by= ws.cell('%s%d' % (col3,max_row)).value
-				#print d
-				ws['%s%d' % (col3,max_row)] = stop_by+1
+			
+			if data == str(x):
+				cell = str(cell)
+				cool=int(remove(cell))
+				stop_by= ws.cell('%s%d' % (col3,cool)).value
+				print stop_by
+				
+				ws['%s%d' % (col3,cool)] = stop_by+1
 				wb.save('testing.xlsx')
 				count+=1
 				
@@ -29,7 +43,7 @@ def search_Student (x):
 		print "This Student is NEW"
 		student_name=raw_input("Enter Name -> ")
 		ws['%s%d' % (col2,insert_name)] =str(student_name)
-		ws['%s%d' % (col,insert_name)] =str(texting)
+		ws['%s%d' % (col,insert_name)] =str(x)
 		ws['%s%d' % (col3,insert_name)] =first_time
 		wb.save('testing.xlsx')
 	else:
@@ -45,7 +59,7 @@ def search_Student (x):
 
 f=open ('lastday.txt', 'r')
 text=f.read()
-texting=text.split('/n')
+texting=text.split('\n')
 #texting ="g"
 
 wb=load_workbook('testing.xlsx',read_only = False, data_only = True)
