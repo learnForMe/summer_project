05ad6382ab1477dbe8587414914c55797acbf592
Gary Tsai <garytsai@Garys-MacBook-Pro.local>
2016-08-02 15:52:04 -0400

    New HID flow

--- header.py
+++ header.py
@@ -11,9 +11,6 @@ def header():
 	wb=load_workbook('testing.xlsx',read_only = False, data_only = True)
 	sheet =wb.get_sheet_by_name('Sheet')
 	ws=wb.active
-	col=get_column_letter(1)# convert column number to letter and use for first column (ID card data)
-	col2=get_column_letter(2)# use for second column (Student name)
-	col3=get_column_letter(3)#use for third column(occurance)
 	sheet['a1'].font=italic24Font
 	sheet['b1'].font=italic24Font
 	sheet.column_dimensions['A'].width = 20
