ac1f710d0781c91dcc9adef37e506a537fbea810
garymalaysia <gtsai70@gmail.com>
2016-08-04 14:10:13 -0400

    not use

--- pythoncard.py
+++ pythoncard.py
@@ -79,7 +79,7 @@ def search_Student (x):
         #print('\a\a')
         again=ws.cell('%s%d' % (col2,cool)).value
         print ("Welcome Back!", again)
-        os.system ('say What is up%s' % again)  # use for prank (aka April Foo)
+        os.system ('say Hellooo%s' % again)  # use for prank (aka April Foo)
   
 
 def formular():
