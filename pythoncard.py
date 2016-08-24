549e0befda49e74efb9e9f7def6e7b17bacde068
garymalaysia <gtsai70@gmail.com>
2016-08-24 17:55:05 -0400

    commented out last part of else statement in pythoncard.py

--- pythoncard.py
+++ pythoncard.py
@@ -121,6 +121,6 @@ while __name__ == '__main__':
         os.system ('clear')
         print (alert.stop)
         os.system ('say Try Again')
-        cs.connection.disconnect()
+       # cs.connection.disconnect()
     
    
