f7b014e234baa599f34b310583bea9ff24393264
garymalaysia <gtsai70@gmail.com>
2016-08-26 13:08:50 -0400

    added timer.py to bypass user input but traffic is still being documented. Modify searchstudent.py to include new timer feature

---
+++ timer.py
@@ -0,0 +1,32 @@
+from time import sleep
+import openpyxl
+from openpyxl import load_workbook
+from openpyxl.compat import range
+from openpyxl.utils import get_column_letter, column_index_from_string
+from openpyxl.utils import coordinate_from_string
+
+def prompt_with_timeout():
+  print ("\t######### MISSING Name or Email ##########\n")
+
+  print('Please press Ctrl-C to enter Name and Email')
+  try:
+    for i in range(0, 10): # 30 minutes is 30*60 seconds
+      sleep(1)
+    print("No Name or Email was logged")
+    n = ""
+    e = ""
+    #print (n)
+    #print (e)
+    #print (n , " and " , e ," added to database")
+  except KeyboardInterrupt:
+    n= input("Student's Name -> ")
+    e = input("Student's Email -> ")
+    print (n + " and " + e +" added to database")
+  return n, e
+
+
+
+
+
+
+
