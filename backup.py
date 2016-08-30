a3c4763cf94fd02f8774a55429409baeb6330294
garymalaysia <gtsai70@gmail.com>
2016-08-30 13:06:59 -0400

    removed old excel sheet

--- backup.py
+++ backup.py
@@ -8,7 +8,7 @@ def back_up():
 	interval = strftime("%M:%S")
 	time = strftime("%Y-%m-%d %H:%M:%S")
 	shutil.copy('/Users/garytsai/Desktop/rfid-reader-http/summer_project/hi.txt', '/Users/garytsai/Desktop/rfid-reader-http/summer_project/back_up')
-	shutil.move('/Users/garytsai/Desktop/rfid-reader-http/summer_project/back_up/hi.txt', '/Users/garytsai/Desktop/rfid-reader-http/summer_project/back_up/hi.txt'% time)
+	#shutil.move('/Users/garytsai/Desktop/rfid-reader-http/summer_project/back_up/hi.txt', '/Users/garytsai/Desktop/rfid-reader-http/summer_project/back_up/hi.txt'% time)
 	#print (interval)
 	threading.Timer(60*60, back_up).start()
 
