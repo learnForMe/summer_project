3d7df6b34a6843f0fb9831c3b7f1b219c826efaf
John Jay <johnjayveterans@Johns-iMac.local>
2017-01-12 12:46:58 -0500

    added email feature

--- rsa.py
+++ rsa.py
@@ -4,8 +4,8 @@ from array import *
 
 #Author: Gary Tsai
 
-#with open('/Users/johnjayveterans/Downloads/pyscard-1.9.4/sudoEng.txt', 'r') as sha:
-with open('/Users/garytsai/Desktop/Fall 2015/Crypto/Portfolio/AllEng.txt', 'r') as sha:
+with open('/Users/johnjayveterans/Desktop/pyscard-1.9.4/sudoEng.txt', 'r') as sha:
+#with open('/Users/garytsai/Desktop/Fall 2015/Crypto/Portfolio/AllEng.txt', 'r') as sha:
 	hashed=sha.read()
 	hashed=hashed.split()
 
@@ -13,9 +13,11 @@ def passwd(x):
 	for line in hashed:
 		hash_object = hashlib.sha512(line.encode('utf-8'))
 		hex_dig = hash_object.hexdigest()
+		#print (hex_dig, line)
 		if hex_dig == x:
-			#print line
-			#print hex_dig
+			#print (line)
+			#print (hex_dig)
 			return line
 
 sha.closed
+
